f0ae9e1c19a609afe866db4223561356a9eb07dc
Juan David Perez <juandavidperezvergara@gmail.com>
2023-09-18 19:23:02 -0500

    Añadiendo el componente de busqueda de pokemones

--- pokedex-app/src/components/Search.jsx
+++ pokedex-app/src/components/Search.jsx
@@ -1,30 +1,100 @@
 import React, { useState, useEffect } from "react";
 import {
+  ScrollView,
   View,
   Text,
   TextInput,
   StyleSheet,
   TouchableOpacity,
+  Image,
   Dimensions,
 } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import { Ionicons, FontAwesome5 } from "@expo/vector-icons";
 
 const windowWidth = Dimensions.get("window").width;
+const typesPokemon = {
+  color: {
+    normal: "#A8A77A",
+    fire: "#EE8130",
+    water: "#6390F0",
+    electric: "#F7D02C",
+    grass: "#7AC74C",
+    ice: "#96D9D6",
+    fighting: "#C22E28",
+    poison: "#A33EA1",
+    ground: "#E2BF65",
+    flying: "#A98FF3",
+    psychic: "#F95587",
+    bug: "#A6B91A",
+    rock: "#B6A136",
+    ghost: "#735797",
+    dragon: "#6F35FC",
+    dark: "#705746",
+    steel: "#B7B7CE",
+  },
+  nameIcon: {
+    normal: "body",
+    fire: "flame",
+    water: "water",
+    electric: "flash",
+    grass: "leaf",
+    ice: "snow",
+    fighting: "fist",
+    poison: "skull",
+    ground: "earth",
+    flying: "airplane",
+    psychic: "eye",
+    bug: "bug",
+    rock: "skull",
+    ghost: "ghost",
+    dragon: "dragon",
+    dark: "moon",
+    steel: "cube",
+  },
+};
 
 const Search = ({ navigation }) => {
-  const [pokemon, setPokemon] = useState(null);
+  const [input, setInput] = useState(null);
   const [datos, setDatos] = useState(null);
 
+  //una funcion que toma el nombre del pokemon y hace una peticion a la api
+  const getPokemon = async () => {
+    try {
+      const response = await fetch(
+        "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0"
+      );
+      if (!response.ok) throw "Error al obtener los datos";
+      const data = await response.json();
+      const results = data.results;
+
+      const filteredResults = results.filter((pokemon) => {
+        return pokemon.name.includes(input.toLowerCase());
+      });
+
+      const pokemonData = await Promise.all(
+        filteredResults.slice(0, 10).map(async (pokemon) => {
+          const pokemonRecord = await fetch(pokemon.url);
+          if (!pokemonRecord.ok) throw "Error al obtener los datos";
+          return pokemonRecord.json();
+        })
+      );
+
+      setDatos(pokemonData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <View style={styles.searchContainer}>
         <TextInput
           style={styles.searchInput}
           placeholder="Search Pokemon"
           placeholderTextColor="#000"
-          onChangeText={(text) => setPokemon(text)}
+          onChangeText={(text) => setInput(text)}
         />
-        <TouchableOpacity style={styles.searchButton}>
+        <TouchableOpacity onPress={() => getPokemon()}>
           <Ionicons
             name="search"
             size={24}
@@ -33,7 +103,59 @@ const Search = ({ navigation }) => {
           />
         </TouchableOpacity>
       </View>
-    </View>
+      <View style={styles.pokemonContainer}>
+        {datos && (
+          <View style={styles.pokemonList}>
+            {datos.map((pokemon) => {
+              return (
+                <TouchableOpacity key={pokemon.id} style={styles.pokemon}>
+                  <Text style={styles.pokemonName}>{pokemon.name}</Text>
+                  <Text style={styles.pokemonId}>#{pokemon.id}</Text>
+                  <Image
+                    source={{ uri: pokemon.sprites.front_default }}
+                    style={{ width: 100, height: 100 }}
+                  />
+                  <View style={{ flexDirection: "row" }}>
+                    {pokemon.types.map((type) => {
+                      return (
+                        <View
+                          key={type.type.name}
+                          style={{
+                            backgroundColor: typesPokemon.color[type.type.name],
+                            borderRadius: 5,
+                            padding: 5,
+                            margin: 5,
+                            flexDirection: "row",
+                            alignItems: "center",
+                          }}
+                        >
+                          {type.type.name === "dragon" ? (
+                            <FontAwesome5
+                              name="dragon"
+                              size={16}
+                              color="#fff"
+                            />
+                          ) : (
+                            <Ionicons
+                              name={typesPokemon.nameIcon[type.type.name]}
+                              size={16}
+                              color="#fff"
+                            />
+                          )}
+                          <Text style={{ color: "#fff" }}>
+                            {type.type.name}
+                          </Text>
+                        </View>
+                      );
+                    })}
+                  </View>
+                </TouchableOpacity>
+              );
+            })}
+          </View>
+        )}
+      </View>
+    </ScrollView>
   );
 };
 
@@ -41,10 +163,10 @@ const styles = StyleSheet.create({
   container: {
     height: "100%",
     backgroundColor: "#FF6961",
-    alignItems: "center",
   },
   searchContainer: {
     flexDirection: "row",
+    alignSelf: "center",
     alignItems: "center",
     justifyContent: "space-between",
     marginTop: 50,
@@ -54,8 +176,36 @@ const styles = StyleSheet.create({
     borderRadius: 15,
   },
   searchInput: {
+    width: "80%",
     paddingHorizontal: 20,
     fontSize: 18,
+    fontWeight: "bold",
+  },
+  pokemonContainer: {
+    marginTop: 50,
+    alignItems: "center",
+  },
+  pokemonList: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+  },
+  pokemon: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: 170,
+    height: 200,
+    backgroundColor: "#fff",
+    borderRadius: 15,
+    marginVertical: 10,
+    marginHorizontal: 7,
+  },
+  pokemonName: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  pokemonId: {
+    fontSize: 14,
     fontWeight: "bold",
   },
 });
